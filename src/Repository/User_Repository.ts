45bdbdb025b0cbdfb22159c111a993d4b97ba55c
Kevin Denker <oficialdenker@gmail.com>
2024-02-20 09:22:17 -0300

    New function findUser

--- src/Repository/User_Repository.ts
+++ src/Repository/User_Repository.ts
@@ -123,6 +123,24 @@ class UserRepository {
       }
     }
   }
+
+  public async findUser(id: string) {
+    try {
+      const user = await User.findOne({ id_: id });
+
+      if (user == null) {
+        throw new Error("User not exists");
+      }
+
+      return user;
+    } catch (error) {
+      if (error instanceof Error) {
+        return { error: `${error.message}` };
+      } else {
+        return { error: "error" };
+      }
+    }
+  }
 }
 
 export default UserRepository;
