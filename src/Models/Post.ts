c18edb5631adb5f9122c73443f7af89c35bf057b
Kevin Denker <oficialdenker@gmail.com>
2024-03-01 08:46:20 -0300

    Update Post.ts

--- src/Models/Post.ts
+++ src/Models/Post.ts
@@ -1,22 +1,28 @@
 import mongoose from "mongoose";
-import User from "./User";
 import Artist from "./Artist";
 import Music from "./Music";
+import simpleUser from "./User_Models/SimpleUser";
 
 const Schema = mongoose.Schema;
 
-const Data = new Schema({
-  title: { type: String, required: true },
-  text: { type: String, required: true },
-  aval: { type: Number, required: true },
-  date: { type: Date, default: Date.now(), required: true },
-});
+const Data = new Schema(
+  {
+    title: { type: String, required: true },
+    text: { type: String, required: true },
+    aval: { type: Number, required: true },
+    date: { type: Date, default: Date.now(), required: true },
+  },
+  { _id: false }
+);
 
-const Post = new Schema({
-  user: { type: User, default: {} },
-  data: { type: Data, default: {} },
-  music: { type: Music, default: {} },
-  artist: { type: Artist, default: {} },
-});
+const Post = new Schema(
+  {
+    user: { type: simpleUser, default: {}, required: true },
+    data: { type: Data, default: {}, required: true },
+    music: { type: Music, default: {}, required: true },
+    artist: { type: Artist, default: {}, required: true },
+  },
+  { _id: false }
+);
 
 export default mongoose.model("Post", Post);
