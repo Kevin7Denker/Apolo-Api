d14706be4c43fc57959615fc234d48f72edef6ec
Kevin Denker <oficialdenker@gmail.com>
2024-03-04 18:45:01 -0300

    Update User_Validation.ts

--- src/Utils/Validation/User_Validation.ts
+++ src/Utils/Validation/User_Validation.ts
@@ -2,37 +2,37 @@ import { Response } from "express";
 
 class UserValidation {
   public vefName(value: string, res: Response) {
-    if (value === null) {
+    if (!value) {
       return res.status(422).json({ msg: "The [name] field is required" });
     }
   }
 
   public vefSurname(value: string, res: Response) {
-    if (value === null) {
+    if (!value) {
       return res.status(422).json({ msg: "The [surname] field is required" });
     }
   }
 
   public vefEmail(value: string, res: Response) {
-    if (value === null) {
+    if (!value) {
       return res.json({ msg: "The [email] field is required" });
     }
   }
 
   public vefPhone(value: string, res: Response) {
-    if (value === null) {
+    if (!value) {
       return res.status(422).json({ msg: "The [phone] field is required" });
     }
   }
 
   public vefPassword(value: string, res: Response) {
-    if (value === null) {
+    if (!value) {
       return res.status(422).json({ msg: "The [password] field is required" });
     }
   }
 
   public vefConfirmPassword(value: string, res: Response) {
-    if (value === null) {
+    if (!value) {
       return res
         .status(422)
         .json({ msg: "The [Confirm Password] field is required" });
