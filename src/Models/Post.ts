5b1b04061db1fa4abf52ae66b4e21d11a9f5d338
Kevin Denker <oficialdenker@gmail.com>
2024-03-04 12:47:07 -0300

    Update Post.ts

--- src/Models/Post.ts
+++ src/Models/Post.ts
@@ -14,10 +14,10 @@ const Data = new Schema(
 
 const Post = new Schema(
   {
-    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    userId: { type: Schema.Types.ObjectId, required: true },
     data: { type: Data, default: {}, required: true },
-    musicId: { type: Schema.Types.ObjectId, ref: "Music", required: true },
-    artistId: { type: Schema.Types.ObjectId, ref: "Artist", required: true },
+    musicId: { type: Schema.Types.ObjectId, required: true },
+    artistId: { type: Schema.Types.ObjectId, required: true },
   },
   { _id: false }
 );
