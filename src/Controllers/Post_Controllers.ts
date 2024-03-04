f78b30ac3c7acc16ed764b4d2b34fdcfbcfe53aa
Kevin Denker <oficialdenker@gmail.com>
2024-03-04 18:35:51 -0300

    Update Post_Controllers.ts

--- src/Controllers/Post_Controllers.ts
+++ src/Controllers/Post_Controllers.ts
@@ -1,19 +1,31 @@
-import PostRepository from "../Repository/Post_Repository";
-
+import { SendPostRequest } from "../Interface/Requests/Post_Request";
 import { Request, Response } from "express";
+
+import PostRepository from "../Repository/Post_Repository";
+import PostValidation from "../Utils/Validation/Post_Validation";
 
 class PostController {
   private postRepository: PostRepository;
+  private postValidation: PostValidation;
 
-  constructor(postRepository: PostRepository) {
+  constructor(postRepository: PostRepository, postValidation: PostValidation) {
     this.postRepository = postRepository;
+    this.postValidation = postValidation;
   }
 
   public async sendPost(req: Request, res: Response) {
-    const { title, text, aval, userId, musicId, artistId } = req.body;
+    const { title, text, aval, userId, musicId, artistId }: SendPostRequest =
+      req.body;
 
     try {
-      const response = await this.postRepository.sendPost(
+      this.postValidation.vefTitle(title, res);
+      this.postValidation.vefText(text, res);
+      this.postValidation.vefAval(aval, res);
+      this.postValidation.vefUserId(userId, res);
+      this.postValidation.vefArtistId(artistId, res);
+      this.postValidation.vefMusicId(musicId, res);
+
+      await this.postRepository.sendPost(
         title,
         text,
         aval,
@@ -22,9 +34,7 @@ class PostController {
         artistId
       );
 
-      res
-        .status(201)
-        .json({ success: true, message: "Post Created", post: response });
+      res.status(201).json({ success: true, message: "Post Created" });
     } catch (error: unknown) {
       if (error instanceof Error) {
         res.status(404).json({
