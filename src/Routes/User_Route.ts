4da96fe312013071369814960b2b5af3d89d9032
Kevin Denker <oficialdenker@gmail.com>
2024-03-04 18:36:21 -0300

    Update User_Route.ts

--- src/Routes/User_Route.ts
+++ src/Routes/User_Route.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import UserController from "../Controllers/User_Controllers";
 import UserRepository from "../Repository/User_Repository";
+import UserValidation from "../Utils/Validation/User_Validation";
 const route = express.Router();
 
 const userRepository = new UserRepository();
-const userController = new UserController(userRepository);
+const userValidation = new UserValidation();
+
+const userController = new UserController(userRepository, userValidation);
 
 route.get("/find-user=:id", (req, res) => userController.findUser(req, res));
 
