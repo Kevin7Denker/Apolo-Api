315bdd16d47d35a18a69c617b4791acaa4eb6c82
Kevin Denker <oficialdenker@gmail.com>
2024-03-04 18:36:17 -0300

    Update Post_Route.ts

--- src/Routes/Post_Route.ts
+++ src/Routes/Post_Route.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import PostRepository from "../Repository/Post_Repository";
 import PostController from "../Controllers/Post_Controllers";
+import PostValidation from "../Utils/Validation/Post_Validation";
 import AuthMiddleWare from "../MiddleWare/Auth_MiddleWare";
 
 const route = express.Router();
 
 const postRepository = new PostRepository();
-const postController = new PostController(postRepository);
+const postValidation = new PostValidation();
+
+const postController = new PostController(postRepository, postValidation);
 
 route.post("/send", AuthMiddleWare.checkToken, (req, res) =>
   postController.sendPost(req, res)
