092df2ac144f007ac7e21cdd4d8e817127324534
Kevin Denker <oficialdenker@gmail.com>
2024-03-04 18:36:31 -0300

    Create Post_Validation.ts

---
+++ src/Utils/Validation/Post_Validation.ts
@@ -0,0 +1,41 @@
+import { Response } from "express";
+
+class PostValidation {
+  public vefTitle(value: string, res: Response) {
+    if (value === null) {
+      return res.status(422).json({ msg: "The [title] field is required" });
+    }
+  }
+
+  public vefText(value: string, res: Response) {
+    if (value === null) {
+      return res.status(422).json({ msg: "The [text] field is required" });
+    }
+  }
+
+  public vefAval(value: number, res: Response) {
+    if (value === null) {
+      return res.status(422).json({ msg: "The [aval] field is required" });
+    }
+  }
+
+  public vefUserId(value: string, res: Response) {
+    if (value === null) {
+      return res.status(422).json({ msg: "The [UserId] field is required" });
+    }
+  }
+
+  public vefMusicId(value: string, res: Response) {
+    if (value === null) {
+      return res.status(422).json({ msg: "The [MusicId] field is required" });
+    }
+  }
+
+  public vefArtistId(value: string, res: Response) {
+    if (value === null) {
+      return res.status(422).json({ msg: "The [ArtistId] field is required" });
+    }
+  }
+}
+
+export default PostValidation;
