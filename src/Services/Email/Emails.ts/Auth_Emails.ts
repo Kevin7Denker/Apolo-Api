1b293bb96f895f1721a18ef0f845718e4c7c069d
Kevin Denker <oficialdenker@gmail.com>
2024-06-22 18:46:33 -0300

    Create Auth_Emails.ts

---
+++ src/Services/Email/Emails.ts/Auth_Emails.ts
@@ -0,0 +1,26 @@
+import resend from "../Config/Email";
+import ejs from "ejs";
+import fs from "fs";
+import path from "path";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export async function SignUpEmail(email: string, token: string) {
+  const templatePath = path.join(
+    __dirname,
+    "../../Templates/Emails/Email_Validation.html"
+  );
+  const templateContent = fs.readFileSync(templatePath, "utf-8");
+
+  const html = await ejs.render(templateContent, {
+    verificationLink: `https://apolo-api.onrender.com/auth/verify-email/${token}`,
+  });
+
+  resend.emails.send({
+    from: `${process.env.RESEND_EMAIL}`,
+    to: `${email}`,
+    subject: "Please verify your email address",
+    html: html,
+  });
+}
