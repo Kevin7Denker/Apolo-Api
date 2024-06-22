4f814e4080b128ec446cf5a8242c6a7ca00009ff
Kevin Denker <oficialdenker@gmail.com>
2024-06-22 18:44:22 -0300

    Removendo interfaces

--- src/Interface/Requests/Post_Request.ts
+++
@@ -1,8 +0,0 @@
-export interface SendPostRequest {
-  title: string;
-  text: string;
-  aval: number;
-  userId: string;
-  musicId: string;
-  artistId: string;
-}
--- src/Interface/Requests/User_Request.ts
+++
@@ -1,8 +0,0 @@
-export interface SignUpRequest {
-  name: string;
-  surname: string;
-  email: string;
-  phone: string;
-  password: string;
-  confirmPassword: string;
-}
