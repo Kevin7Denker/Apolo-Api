58caaf347f293f55a4cd0c0d75003e2c3f23e3cd
Kevin Denker <oficialdenker@gmail.com>
2024-06-24 23:56:27 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,13 @@
-import express from "express";
-import cors from "cors";
-
+import app from "./app";
 import startServer, { env } from "./Config/ServerConfig";
 
-import publicRoutes from "./Routes/Publics/Public_Route";
-import authRoutes from "./Routes/Auth/Auth_Route";
-import userRoutes from "./Routes/Main/User_Route";
-import postRoutes from "./Routes/Main/Post_Route";
-import assetsRoutes from "./Routes/Assets/Images_Route";
-
-const app = express();
 const port = env.PORT || 2612;
-
-app.use(cors());
-app.use(express.json());
-
-app.options("*", cors());
-
-app.use("/", publicRoutes);
-app.use("/auth", authRoutes);
-app.use("/user", userRoutes);
-app.use("/post", postRoutes);
-app.use("/assets", assetsRoutes);
 
 startServer()
   .then(() => {
-    app.listen(port);
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
   })
   .catch((error) => {
     console.log("Erro: " + error);
