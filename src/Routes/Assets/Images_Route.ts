ed70f8f984120b9d8be7808be154282c3a542555
Kevin Denker <oficialdenker@gmail.com>
2024-06-24 23:56:20 -0300

    Update Images_Route.ts

--- src/Routes/Assets/Images_Route.ts
+++ src/Routes/Assets/Images_Route.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 const route = express.Router();
 
-route.get("/logo", (req, res) => {
+route.get("/logo", (_req, res) => {
   const imagePath = path.join(__dirname, "../Assets/Images/Logos/Logo.png");
   res.type("image/jpeg");
   res.sendFile(imagePath);
