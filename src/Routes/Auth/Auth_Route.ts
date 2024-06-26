a5e9ce9e99810db2d78f58cd2447df5e3d10926f
Kevin Denker <oficialdenker@gmail.com>
2024-06-26 00:15:23 -0300

    Update Auth_Route.ts

--- src/Routes/Auth/Auth_Route.ts
+++ src/Routes/Auth/Auth_Route.ts
@@ -14,12 +14,15 @@ route.post("/signin", (req, res) => authController.signIn(req, res));
 route.get("/verify-email/:token", (req, res) =>
   authController.valEmail(req, res)
 );
-
 route.get("/verify-email/error/:token", (req, res) =>
   authController.errorValEmail(req, res)
 );
 route.get("/verify-email/resend/:token", (req, res) =>
   authController.resendValEmail(req, res)
+);
+
+route.delete("/user/:userId", (req, res) =>
+  authController.deleteUser(req, res)
 );
 
 export default route;
