6920336e6bac72fa7f18e3bc23dacbf6f70ee3d8
Kevin Denker <oficialdenker@gmail.com>
2024-06-29 17:54:56 -0300

    Delete Public_Route.ts

--- src/Routes/Publics/Public_Route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const route = express.Router();
-
-route.get("/", (_req, res) => {
-  res.status(200).json({ msg: "Welcome to Apolo " });
-});
-
-export default route;
