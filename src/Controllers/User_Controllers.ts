1f5fb12e15d846ec3d8a3e12e88c2bc7e68a0bf3
Kevin Denker <oficialdenker@gmail.com>
2024-07-31 08:49:36 -0300

    Update User_Controllers.ts

--- src/Controllers/User_Controllers.ts
+++ src/Controllers/User_Controllers.ts
@@ -24,6 +24,28 @@ class UserController {
       return error;
     }
   }
+
+  public async findIdentity(req: Request, res: Response) {
+    const identity = req.params.identity;
+
+    if (identity === undefined) {
+      return res.status(400).json({ error: "Invalid Identity" });
+    }
+
+    try {
+      const response = await this.userRepository.findIdentity(identity);
+
+      if (response === false) {
+        return res.status(200).json({ pass: response });
+      }
+
+      if (response === true) {
+        return res.status(400).json({ error: "Identity Exists" });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
 }
 
 export default UserController;
