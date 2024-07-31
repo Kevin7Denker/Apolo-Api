4c102615341b2d0664e1c1160f5d28ed39394028
Kevin Denker <oficialdenker@gmail.com>
2024-07-31 12:28:16 -0300

    Update UserController.test.ts

--- src/Tests/UserController.test.ts
+++ src/Tests/UserController.test.ts
@@ -1,31 +1,10 @@
 import axios from "axios";
-import User from "../Models/User";
 
 jest.mock("../Repository/User_Repository.ts");
 
 describe("UserController", () => {
   beforeEach(() => {
     jest.clearAllMocks();
-  });
-
-  describe("findUserById", () => {
-    it("should find user by id successfully", async () => {
-      const user = await User.findOne({
-        "profile.email": "existing@example.com",
-      });
-
-      console.log(user?._id);
-
-      const res = await axios.post(
-        "https://apolo-api.onrender.com/user/find-user/",
-        {
-          id: user,
-        }
-      );
-
-      expect(res.status).toBe(200);
-      expect(res.status).toEqual(200);
-    }, 15000);
   });
 
   describe("findUserIdentity", () => {
