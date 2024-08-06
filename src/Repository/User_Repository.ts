aacd634b79b2d38a164ff9666fdc09e62a92d4ca
Kevin Denker <oficialdenker@gmail.com>
2024-08-06 11:14:11 -0300

    Update User_Repository.ts

--- src/Repository/User_Repository.ts
+++ src/Repository/User_Repository.ts
@@ -1,11 +1,29 @@
 import User from "../Models/User";
 
 class UserRepository {
-  public async findUser(id: string) {
+  public async findUserById(id: string) {
     try {
       const user = await User.findOne({ _id: id });
 
-      if (user == null) {
+      if (user === null) {
+        throw new Error("User not exists");
+      }
+
+      return user;
+    } catch (error) {
+      if (error instanceof Error) {
+        return { error: `${error.message}` };
+      } else {
+        return { error: "error" };
+      }
+    }
+  }
+
+  public async findUserByEmail(email: string) {
+    try {
+      const user = await User.findOne({ "profile.email": email });
+
+      if (user === null) {
         throw new Error("User not exists");
       }
 
