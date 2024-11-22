fbcd25cdde9c3f1a7e8ff8ce9abff15f0eb9dbde
Kevin Denker <oficialdenker@gmail.com>
2024-11-22 08:28:24 -0300

    Feat CompleteWelcome

--- src/Routes/Auth/Auth_Route.ts
+++ src/Routes/Auth/Auth_Route.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Request, Response } from "express";
 
 import AuthRepository from "../../Repository/Auth_Repository";
 import AuthController from "../../Controllers/Auth_Controllers";
@@ -8,24 +9,32 @@ const route = express.Router();
 const authRepository = new AuthRepository();
 const authController = new AuthController(authRepository);
 
-route.post("/signup", (req, res) => authController.signUp(req, res));
-route.post("/signin", (req, res) => authController.signIn(req, res));
+route.post("/signup", (req: Request, res: Response) =>
+  authController.signUp(req, res)
+);
+route.post("/signin", (req: Request, res: Response) =>
+  authController.signIn(req, res)
+);
 
-route.post("/complete-welcome", (req, res) => {
+route.post("/complete-welcome", (req: Request, res: Response) => {
   authController.completeWelcome(req, res);
 });
 
-route.get("/verify-email/:token", (req, res) =>
+route.post("/update-image", (req: Request, res: Response) => {
+  authController.updateImage(req, res);
+});
+
+route.get("/verify-email/:token", (req: Request, res: Response) =>
   authController.valEmail(req, res)
 );
-route.get("/verify-email/error/:token", (req, res) =>
+route.get("/verify-email/error/:token", (req: Request, res: Response) =>
   authController.errorValEmail(req, res)
 );
-route.get("/verify-email/resend/:token", (req, res) =>
+route.get("/verify-email/resend/:token", (req: Request, res: Response) =>
   authController.resendValEmail(req, res)
 );
 
-route.delete("/user/:userId", (req, res) =>
+route.delete("/user/:userId", (req: Request, res: Response) =>
   authController.deleteUser(req, res)
 );
 
--- src/Validators/Auth.ts
+++ src/Validators/Auth.ts
@@ -2,26 +2,37 @@ import { z } from "zod";
 
 class AuthValidator {
   public SignUpBodySchema = z.object({
-    name: z.string(),
-    surname: z.string(),
-    email: z.string().email(),
-    phone: z.string(),
+    name: z.string().min(1),
+    surname: z.string().min(1),
+    email: z.string().email().min(1),
+    phone: z.string().min(1),
     password: z.string().min(6),
-    confirmPassword: z.string().min(6),
+    confirmPassword: z.string().min(6).min(1),
   });
 
   public SignInBodySchema = z.object({
-    email: z.string().email(),
+    email: z.string().email().min(1),
     password: z.string().min(6),
   });
 
   public WelcomeBodySchema = z.object({
-    email: z.string().email(),
+    email: z.string().email().min(1),
     image: z.any(),
-    identity: z.string().max(25),
-    genres: z.array(z.string()),
-    country: z.string(),
-    code: z.string().length(2),
+    identity: z.string().max(25).min(1),
+    genres: z.array(z.string()).min(1),
+    country: z.string().min(1),
+    code: z.string().length(2).min(1),
+  });
+
+  public UserIdBodySchema = z.object({ userId: z.string().min(1) });
+
+  public TokenBodySchema = z.object({
+    token: z.string().min(1),
+  });
+
+  public UpdateImageBodySchema = z.object({
+    image: z.any(),
+    userId: z.string().min(1),
   });
 }
 
